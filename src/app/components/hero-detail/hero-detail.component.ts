a4ece79ed5c9c37091101539778366075f3321b9
Phung Vu Tran Kim <vtkp2002dn@gmail.com>
2019-03-27 12:57:12 +0700

    Implement routing. Add dashboard view and hero detail view

---
+++ src/app/components/hero-detail/hero-detail.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import {Hero} from '../../class/hero'
+import {ActivatedRoute} from "@angular/router"
+import {Location} from "@angular/common"
+import {HeroService} from "../../services/hero.service"
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+  hero: Hero
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) { }
+
+  ngOnInit() {
+    this.getHero()
+  }
+  getHero(): void {
+    //take id convert to number, id here is number. In normal case, id is string
+    const id = +this.activateRoute.snapshot.paramMap.get('id')
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero)
+  }
+  goBack(){
+    this.location.back()
+  }
+
+}
--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -10,15 +10,11 @@ export class HeroesComponent implements OnInit {
   constructor(private heroService: HeroService) {
 
   }
-  selectedHero: Hero
   heroes: Hero[]
   ngOnInit() {
     this.getHeroes()
   }
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)
-  }
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero
   }
 }
